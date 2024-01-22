73cf0c64650220331550ba805c5a33b18fd527bf
YinkaOlusola <pastorcooler@gmail.com>
2024-01-22 11:35:59 -0800

    Catching ValueError and TypeError using Exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """Prints x elements of a list
+
+    args:
+            value: Integer to be printed
+
+            Return: True or False
+
+    """
+
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
+
+        
