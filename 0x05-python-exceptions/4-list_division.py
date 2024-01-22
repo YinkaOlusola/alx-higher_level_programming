127205ffe8df65ea47f9edc5dbe1892470d1793c
YinkaOlusola <pastorcooler@gmail.com>
2024-01-22 15:49:43 -0800

    Fixing requirements error in element-wise division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,22 +4,22 @@ def list_division(my_list_1, my_list_2, list_length):
     """Divides two lists element by element
 
     args:
-            my_list_1: First list
-            my_list_2: Second list
-            list_length: Length of the list to be returned
+            my_list_1 (list): First list
+            my_list_2 (list): Second list
+            list_length (int): Length of the list to be returned
 
-            Return: A new list of length list_length
+            Returns: A new list of length list_length
     """
     result_list = []
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
-            result = 0
             print("wrong type")
-        except ZeroDivisionError:
             result = 0
+        except ZeroDivisionError:
             print("division by 0")
+            result = 0
         except IndexError:
             print("out of range")
             result = 0
