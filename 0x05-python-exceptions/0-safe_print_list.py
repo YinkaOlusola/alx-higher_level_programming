c5dc53494e310bc7f91228b031c1db0ecbde964e
YinkaOlusola <pastorcooler@gmail.com>
2024-01-22 12:41:07 -0800

    Fixing requirements bugs in task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -11,7 +11,6 @@ def safe_print_list(my_list=[], x=0):
         The number of elements printed.
 
     """
-
     return_ = 0
 
     for i in range(x):
@@ -21,5 +20,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    return (return_)
-            
+    return (return_)  
