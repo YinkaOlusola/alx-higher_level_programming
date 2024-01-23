f0dd6326a44f65c62004e1f28f15684337cec99c
YinkaOlusola <pastorcooler@gmail.com>
2024-01-23 15:12:49 -0800

    Fixing coding style bug in task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,7 +7,7 @@ class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Initialize a new Square.
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
@@ -18,7 +18,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-
     def area(self):
-        """Returns the area of the square."""
-        return self.__size**2
+        """Return the area of the square."""
+        return (self.__size**2)
