930aa943e4b63280ddf0f576e5b137af6dd30104
YinkaOlusola <pastorcooler@gmail.com>
2024-01-22 16:01:26 -0800

    Raise name exception

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """Raises a name exception with a message
+
+    args:
+            message: Message in the name exception
+
+    """
+    raise NameError(message)
