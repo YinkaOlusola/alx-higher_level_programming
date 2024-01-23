cbfe6d16029a10c8cd7485559a86abccfaedd0a7
YinkaOlusola <pastorcooler@gmail.com>
2024-01-23 15:32:36 -0800

    Using setters

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,8 +12,14 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        self.size(size)
+        self.size = size
 
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
     def size(self, value):
         """Sets the value of size
 
@@ -24,7 +30,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """Return the area of the square."""
