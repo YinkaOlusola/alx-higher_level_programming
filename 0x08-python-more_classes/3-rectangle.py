316a5b7c7c40edbf73a997c0e01fb47c94b6a54d
YinkaOlusola <pastorcooler@gmail.com>
2024-01-29 17:38:35 -0800

    Printing a rectangle with # - fixing a bug

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -60,5 +60,5 @@ class Rectangle:
         for i in range(self.__height):
             [rectangular_object.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
-                rect.append("\n")
+                rectangular_object.append("\n")
         return ("".join(rectangular_object))
