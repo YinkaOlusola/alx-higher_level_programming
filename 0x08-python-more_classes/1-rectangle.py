7fc707146d3fa780b9ca602ffd4bc04e30158525
YinkaOlusola <pastorcooler@gmail.com>
2024-01-29 16:14:39 -0800

    Fixing a bug in task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Defines a class called rectangle"""
+"""Defines a Rectangle class."""
 
 
 class Rectangle:
-    """Represents a rectangle."""
-    
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
