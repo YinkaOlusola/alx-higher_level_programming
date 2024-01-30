018781d8777c04939da4f4834070616156f28c96
YinkaOlusola <pastorcooler@gmail.com>
2024-01-29 17:33:43 -0800

    Printing a rectangle with # - fixing a bug

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -41,6 +41,16 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    def area(self):
+        """Returns the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return (2 * (self.__width + self.__height))
+
     def __str__(self):
         """Returns a representation of the Rectangle using #"""
         if self._-width == 0 or self.__height == 0:
