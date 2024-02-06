588138a93159fae598ecf2edbceaa308d249a146
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-06 06:43:57 -0800

    Checking if an object is a sublass of a given class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Checks if an object is an instance of an inherited class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Returns True or False if obj is an instance of derived
+    class of is_class.
+
+    Args:
+        obj (object): The object whose instances is to be checked.
+        a_class (object): The parent class to check obj against
+    Returns:
+        True if obj is an instance of a_class or its derive.
+        otherwise - False.
+    """
+    return (issubclass(obj, a_class))
