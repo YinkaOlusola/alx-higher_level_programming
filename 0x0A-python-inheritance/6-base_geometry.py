6bf9df167bb0e770397da43b1871e77f5de0f3ff
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-06 07:19:32 -0800

    Adding a method - area - to the class without implementing it

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""An empty BaseGeometry class."""
+
+
+class BaseGeometry:
+    """An empty BaseGeometry class."""
+
+    def __init__(self):
+        """A method to initilize the attributes of the class"""
+        return
+    
+    def area(self):
+        """A function to implement area."""
+        return
