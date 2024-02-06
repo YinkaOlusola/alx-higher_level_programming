1cbac78d7d116635f02b2ae2314ac4ecc4c9e08d
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-06 04:35:21 -0800

    Setting out on inheritance task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Defines an object that returns a list of available attributes and methods of an object"""
+
+def lookup(obj):
+    """Returns a list of available attributes and methods"""
+    return 0
