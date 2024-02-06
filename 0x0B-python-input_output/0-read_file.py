1634830f6370fbef03da878abff93dfe4460c062
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-06 15:58:20 -0800

    Reading from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """A module that reads from a file"""
 
+
 def read_file(filename=""):
-    """Reads from a file
-    Args:
-        filename (str): The name of the file to be read
-    """
-    with open(filename, encoding='utf-8') as f:
+    """Reads from a file and prints to the stdout"""
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
+        print(read_data, end="")
