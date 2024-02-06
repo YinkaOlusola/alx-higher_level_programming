388d633b74ba8a2af3f1d7d705c7b038a7fa0f23
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-06 07:56:42 -0800

    Raising valueerror and TypeError

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value.
+
+        Args:
+            name (str): Name.
+            value (int): Integer value to be validated.
+        
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0
+        """
+        if not isinstance(self.value, int):
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
