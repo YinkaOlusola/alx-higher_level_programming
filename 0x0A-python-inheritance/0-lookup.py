e2df2138a2b0b9f2e93c41998a5eb699c66b87d4
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-06 04:46:46 -0800

    Returning all attributes and methods of an object - fixing a bug

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 """Defines an object that returns a list of 
 available attributes and methods of an object
 """
 
+
 def lookup(obj):
-    """Returns a list of available attributes and methods"""
+    """Returns a list of object attributes and methods"""
     return (dir(obj))
