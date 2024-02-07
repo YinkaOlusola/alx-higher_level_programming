e3f7f9953827eaf52e27fc0d6c60d59cdd0ebe13
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-07 12:41:03 -0800

    Defining a student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,16 +3,19 @@
 
 
 class Student:
-    """This class defines a student.
+    """This class defines a student."""
 
-    Args:
-        first_name (str): The first name of the student.
-        last_name (str): Last name of the student.
-        age (int): Age of the student.
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """Get a dictionary representation of the student."""
