5334d7e75b21da0b115a060d3af5c792657de15a
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-12 19:14:16 -0800

    Fixing requirement error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,11 +81,11 @@ class Rectangle(Base):
 
     def area(self):
         """Returns the area of the rectangle."""
-        return (self.__width * self.__height)
+        return (self.width * self.height)
 
     def display(self):
         """Prints in stdout the Rectangle instance with the character #"""
-        if self.__width == 0 or self.__height == 0:
+        if self.width == 0 or self.height == 0:
             print("")
             return
 
