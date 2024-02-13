192c16a93fe7fc8bb677a02272f2f8b8df7c5d22
YinkaOlusola <akindeleyinka1005@gmail.com>
2024-02-12 20:36:10 -0800

    json to string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+
 """Defines a class called Base."""
+import json
+import csv
+import turtle
 
 
 class Base:
-    """This is a Base class."""
+    """This is a Base class.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
 
     __nb_objects = 0
 
@@ -17,3 +25,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
